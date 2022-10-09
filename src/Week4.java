56e0dd6f062e7a1044460b7057b4ba6cab8696ee
KhacHieuVNU <20020195@vnu.edu.vn>
2022-10-09 20:20:23 +0700

    test

---
+++ src/Week4.java
@@ -0,0 +1,19 @@
+import java.sql.SQLClientInfoException;
+
+public class Week4 {
+    public static String GiaVe(int a){
+        String s="";
+        if (a < 0 || a > 120) {
+            s = "Độ tuổi không hợp lệ. Xin nhập lại";
+        } else if (a >= 0 && a <= 9) {
+            s = "Miễn Phí";
+        } else if (a >= 10 && a <= 19) {
+            s = "30000 đồng";
+        }else if (a >= 20 && a <= 59) {
+            s = "50000 đồng";
+        }else if (a >= 60 && a <= 120) {
+            s = "30000 đồng";
+        }
+        return s;
+    }
+}
